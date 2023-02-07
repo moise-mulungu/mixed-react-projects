887612cb693d1c75c21e8fb92f76f5884150c680
moise <moisemlg90@gmail.com>
2023-02-07 16:44:47 +0200

    Resolve the sort array task

--- src/utils/array/snippets/sort.js
+++ src/utils/array/snippets/sort.js
@@ -1,5 +1,14 @@
 // howtojs: arrays: sort numbers
 // todoMM: solve this exercise
-;[2, 5, 3, 14, 11].sort(/* google the syntax to sort numbers goes here */)
+const myArray =[2, 5, 3, 14, 11](/* google the syntax to sort numbers goes here */)
+
+// create a function that sorts an array of objects by a given key
+function sortArray(a, b) {
+    return a - b
+}
+
+myArray.join(' ');
+myArray.sort(sortArray).join(' ')
+console.log(myArray)
 
 // todoDM: write exercise to sort array of objects
