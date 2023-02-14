22b12d73a0cdf6662af5ecfb2731e1f091753721
moise <moisemlg90@gmail.com>
2023-02-14 12:51:52 +0200

    Resolve the sort array/objects exercises

--- src/utils/array/snippets/sort-objects.js
+++ src/utils/array/snippets/sort-objects.js
@@ -9,10 +9,14 @@
     objectProperty1: 3,
     objectProperty2: 4,
   },
-]./* your code goes here */
+].sort((a, b) => a.objectProperty2 -b.objectProperty2)
+.forEach((item) => {
+  console.log(`${item.objectProperty2} ${item.objectProperty2}`)
+})
+/* your code goes here */
 
 // expected result:
-[
+;[
 	{
 		objectProperty1: 3,
 		objectProperty2: 4,
@@ -21,4 +25,11 @@
     objectProperty1: 1,
     objectProperty2: 2,
   },
-]
+].sort((a, b) => a.objectProperty2 -b.objectProperty2)
+.forEach((item) => {
+  console.log(`${item.objectProperty2} ${item.objectProperty2}`)
+})
+
+// I used two different methods to sort the array of objects from this source: https://www.javascripttutorial.net/array/javascript-sort-an-array-of-objects/
+// the first method is to use the sort method and pass in a function
+// the second method is to use the forEach method and pass in a function
--- src/utils/array/snippets/sort.js
+++ src/utils/array/snippets/sort.js
@@ -1,6 +1,6 @@
 // howtojs: arrays: sort numbers
 // todoMM: solve this exercise
-const myArray =[2, 5, 3, 14, 11](/* google the syntax to sort numbers goes here */)
+const myArray =[2, 5, 3, 14, 11, 12.4, ]; 
 
 // create a function that sorts an array of objects by a given key
 function sortArray(a, b) {
@@ -8,13 +8,14 @@ function sortArray(a, b) {
 }
 
 // join the array into a string
-myArray.join();
+// let joinedArray = myArray.join();
+// console.log(joinedArray);
 
 // sort the array
-myArray.sort();
+// myArray.sort();
 
 // sort the array using the function
-myArray.sort(sortArray);
-console.log(myArray);
+let sortedArray = myArray.sort(sortArray);
+console.log(sortedArray);
 
 // todoDM: write exercise to sort array of objects
