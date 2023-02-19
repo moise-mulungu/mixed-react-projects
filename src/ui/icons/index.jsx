3ed513ac2c123dc9cc6fe291da7c6601db0fa0e6
moise <moisemlg90@gmail.com>
2023-02-19 19:41:21 +0200

    Create an icon constant

---
+++ src/ui/icons/index.jsx
@@ -0,0 +1,18 @@
+const svgImages = [
+  { id: 'github', name: 'GitHub', class: 'h-6 w-6', fill: 'currentColor', viewBox: '0 0 24 24' },
+  { id: 'linkedin', name: 'LinkedIn', class: 'h-6 w-6', fill: 'currentColor', viewBox: '0 0 24 24' },
+  { id: 'twitter', name: 'Twitter', class: 'h-6 w-6', fill: 'currentColor', viewBox: '0 0 24 24' },
+  { id: 'medium', name: 'Medium', class: 'h-6 w-6', fill: 'currentColor', viewBox: '0 0 24 24' },
+  { id: 'wellfound', name: 'Wellfound', class: 'h-6 w-6', fill: 'currentColor', viewBox: '0 0 24 24' },
+]
+
+svgImages.map((id, name, class, fill, viewBox) => (
+    
+))
+
+export const footerSocialLinksIcons = (props) => {
+    return (
+
+    )
+}
+    
