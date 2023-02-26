32c51f368c10c8de0b7fbefdd1009f0c844128fe
moise <moisemlg90@gmail.com>
2023-02-26 17:44:50 +0200

    make a list of the array methods that mutate

--- project-info/teamdm/tech/javascript/array-methods/mutating-array-methods.js
+++ project-info/teamdm/tech/javascript/array-methods/mutating-array-methods.js
@@ -2,9 +2,35 @@
 //             ex: sort, reverse, push, pop, etc.
 
 //             write a very succinct example of each. ex:
-// sort
+// sort: list the elements of an array in ascending order
 [2, 1].sort() // 1, 2
 
+// reverse: reverse the order of the elements of an array
+[1, 2].reverse() // 2, 1
+
+// push: add an element to the end of an array
+[1, 2].push(3) // 1, 2, 3
+
+// pop: remove an element from the end of an array
+[1, 2, 3].pop() // 1, 2
+
+// shift: remove an element from the beginning of an array
+[1, 2, 3].shift() // 2, 3
+
+// unshift: add an element to the beginning of an array
+[2, 3].unshift(1) // 1, 2, 3
+
+// splice: remove an element from a specific index of an array
+[1, 2, 3].splice(1, 1) // 1, 3
+
+// split: split a string into an array of substrings
+['1,2,3'].split(',') // 1, 2, 3
+
+// join: join the elements of an array into a string
+[1, 2, 3].join(',') // '1,2,3'
+
+// slice: copy a portion of an array
+[1, 2, 3].slice(1, 2) // 2
 
 
 
