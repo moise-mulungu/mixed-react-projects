e2e3b52693120f7165fff4e2624709309ff6b3db
moise <moisemlg90@gmail.com>
2023-02-26 17:21:19 +0200

    Write array codes

--- project-info/teamdm/tech/javascript/array-methods/array-method-exercises/filter-map-reduce-exercises.js
+++ project-info/teamdm/tech/javascript/array-methods/array-method-exercises/filter-map-reduce-exercises.js
@@ -9,14 +9,14 @@
 // array.map is to transform each element of an array into something different
 // array.reduce is to transform an array into something else (ex: into a number, or an object)
 // example exercise: create a new array of the same length, adding 5 to each element
-[1, 2, 3].map(number => number + 5) // [6, 7, 8]
+[1, 2, 3, 0, 0].map(number => number + 5) // [6, 7, 8]
 
 // write code to get 1 number which is the sum of the elements
-[1, 2, 3].
+[1, 2, 3].reduce((sum, number) => sum + number, 0)
 
 // create a new array of the same length, multiplying each element by 10
-[1, 2, 3].
+[1, 2, 3].map(number => number * 10) // [10, 20, 30]
 
 // create a smaller array containing only the values that are less than 3
-[1, 2, 3].
+[1, 2, 3].filter(number => number < 3)
 
