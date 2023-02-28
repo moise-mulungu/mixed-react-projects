b1efc3ff7ceb0656b7f6b826d74e4af43ba7f061
moise <moisemlg90@gmail.com>
2023-02-28 15:19:54 +0200

    Add components to the pages function

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,13 @@
 import Link from '@/ui/link'
+import PortfolioTopLevelComponent from '@/pages/portfolio'
 
 export default function Pages() {
   return (
     <div className="m-4">
       <h1>All the things!</h1>
       {/* DM: todoMM: add links for all items at this same level in the pages dir. ex: portfolio, component-examples (not explorer) */}
+      <PortfolioTopLevelComponent data={data} />
+      <ComponentExamplesPage />
       <Link href="/component-examples">Component Examples</Link>
       <br /> {/* don't do this! */}
       <Link href="/ui-examples">UI Examples</Link>
