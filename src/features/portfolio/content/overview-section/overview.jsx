601b1fc6e2bf2af69858c9e30f908c0a1f57f6c9
moise <moisemlg90@gmail.com>
2023-03-07 13:09:22 +0200

    Add overview texts

--- src/features/portfolio/content/overview-section/overview.jsx
+++ src/features/portfolio/content/overview-section/overview.jsx
@@ -1,9 +1,14 @@
+const headerText = 'Hey there, I am Moise. a software developer';
+const paragraphText = 'I can help you build a product, feature or website Look through some of my work experience! You like what you see and have a project you need coded, do not hesitate to contact me.';
+
 const Overview = (props) => {
     // const { data } = props;
     return (
-        <div className="overview">
-        </div>
-    );
+      <div className="overview">
+        <h2>{headerText}</h2>
+        <p>{paragraphText}</p>
+      </div>
+    )
 };
 
 export default Overview;
