0c93c7f6c6ed653309319e47ccc75a37b640c657
moise <moisemlg90@gmail.com>
2023-03-07 17:27:43 +0200

    code-wars full tests

--- project-info/teamdm/training/js/classes/hero.js
+++ project-info/teamdm/training/js/classes/hero.js
@@ -1,5 +1,25 @@
 // const { assert } = require('chai');
 // describe('hero class', () => {
+//  it('should create a hero', () => {
+//    let myHero = new Hero()
+//    assert.strictEqual(
+//      myHero.name,
+//      'Hero',
+//      'Hero should have a \'name\' attribute with value "Hero"'
+//    )
+//    assert.strictEqual(
+//      myHero.experience,
+//      0,
+//      "Hero should have an 'experience' attribute with value 0"
+//    )
+//    assert.strictEqual(myHero.health, 100, "Hero should have a 'health' attribute with value 100")
+//    assert.strictEqual(
+//      myHero.position,
+//      '00',
+//      'Hero should have a \'position\' attribute with value "00"'
+//    )
+//    assert.strictEqual(myHero.damage, 5, "Hero should have a 'damage' attribute with value 5")
+//  })
 //   it('should use name argument', () => {
 //     assert.strictEqual(
 //       new Hero('Greg').name,
