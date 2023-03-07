5325c9fe4db1d2ad3e4b804fb8786de8f7ebdebd
moise <moisemlg90@gmail.com>
2023-03-07 12:29:28 +0200

    Clean up files

--- src/features/component-examples/avatar-set/code-from-joy-of-react.js
+++ src/features/component-examples/avatar-set/code-from-joy-of-react.js
@@ -1,10 +1,4 @@
-// First challenge
-/* 
-
-
-
-*/
-
+// First challenge: Create a component that renders a set of avatars
 // App.js
 import Avatar from './Avatar'
 
@@ -32,59 +26,3 @@ function App() {
 }
 
 export default App
-
-
-// Avatar.js
-function Avatar({ src, alt }) {
-  return (
-    <button className="avatar-btn">
-      <img
-        className="avatar"
-        src={src}
-        alt={alt}
-      />
-    </button>
-  );
-}
-
-export default Avatar;
-
-
-// solution
-// App.js
-import Avatar from './Avatar';
-
-const data = [
-  {
-    id: '001',
-    alt: 'person with curly hair and a black T-shirt',
-  },
-  {
-    id: '002',
-    alt: 'person wearing a hijab and glasses',
-  },
-  {
-    id: '003',
-    alt: 'person with short hair wearing a blue hoodie',
-  },
-  {
-    id: '004',
-    alt: 'person with a pink mohawk and a raised eyebrow',
-  },
-];
-
-function App() {
-  return (
-    <div className="avatar-set">
-      {data.map(({ id, alt }) => (
-        <Avatar
-          key={id}
-          src={`https://sandpack-bundler.vercel.app/img/avatars/${id}.png`}
-          alt={alt}
-        />
-      ))}
-    </div>
-  );
-}
-
-export default App;
