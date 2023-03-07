2d83536a2db08d4e59de7de81d777deb2cb38d20
moise <moisemlg90@gmail.com>
2023-03-07 14:20:53 +0200

    Add key prop

--- src/features/portfolio/content/overview-section/overview.jsx
+++ src/features/portfolio/content/overview-section/overview.jsx
@@ -1,10 +1,30 @@
-import { contentOverviewHeaderText, contentOverviewParagraphText } from '../content';
+import {
+  contentOverviewHeaderText,
+  contentOverviewParagraphText,
+  footerSocialLinks,
+} from '../../../constants/portfolio'
+
+
 
 
 const Overview = (props) => {
     // const { data } = props;
     return (
       <div className="overview">
+        {footerSocialLinks.map(({ id, name, title, Icon, url }) => {
+            return (
+                <li key={id} className="ml-2 font-medium">
+                    <a
+                      href={url}
+                    >
+                        {name ? name : null}
+                        {Icon ? <Icon title={title} /> : null}
+                    </a>
+                </li>
+            )
+            // MM: toDM: i was getting this error: "Missing key prop for the element iterator". I then added it to the <li></li> element, is it a good practice to add it to the <li></li> element instead of to the <a></a> element?
+        })}
+
         <h2>{contentOverviewHeaderText}</h2>
         <p>{contentOverviewParagraphText}</p>
       </div>
