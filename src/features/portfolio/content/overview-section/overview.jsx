36d8f210c3160854c2f666da9dd464df077aca2d
moise <moisemlg90@gmail.com>
2023-03-08 17:18:47 +0200

    Implement the unordered list tag

--- src/features/portfolio/content/overview-section/overview.jsx
+++ src/features/portfolio/content/overview-section/overview.jsx
@@ -8,18 +8,20 @@ const Overview = (props) => {
   const {} = props
   return (
     <div className="overview">
-      {footerSocialLinks.map(({ id, name, title, Icon, url }) => {
-        return (
-          <li key={id} className="ml-2 font-medium">
-            <a href={url}>
-              {name ? name : null}
-              {Icon ? <Icon title={title} /> : null}
-            </a>
-          </li>
-        )
-        // MM: ???DM: i was getting this error: "Missing key prop for the element iterator". I then added it to the <li></li> element, is it a good practice to add it to the <li></li> element instead of to the <a></a> element?
-        // DM: Yes, you add the key prop to the element that repeats within it's containing element; 'a' does not repeat within it's containing element (which is 'li'), but 'li' repeats within the 'div'.
-      })}
+      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        {footerSocialLinks.map(({ id, name, title, Icon, url }) => {
+          return (
+            <li key={id} className="ml-2 font-medium">
+              <a href={url}>
+                {name ? name : null}
+                {Icon ? <Icon title={title} /> : null}
+              </a>
+            </li>
+          )
+          // MM: ???DM: i was getting this error: "Missing key prop for the element iterator". I then added it to the <li></li> element, is it a good practice to add it to the <li></li> element instead of to the <a></a> element?
+          // DM: Yes, you add the key prop to the element that repeats within it's containing element; 'a' does not repeat within it's containing element (which is 'li'), but 'li' repeats within the 'div'.
+        })}
+      </ul>
 
       <h2>{contentOverviewHeaderText}</h2>
       <p>{contentOverviewParagraphText}</p>
