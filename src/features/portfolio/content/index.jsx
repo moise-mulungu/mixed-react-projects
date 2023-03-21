08e8394c9ed3e03daf09398f876b38ecf3dc602c
moise <moisemlg90@gmail.com>
2023-03-21 14:35:48 +0200

    Import the skill component

--- src/features/portfolio/content/index.jsx
+++ src/features/portfolio/content/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Overview from './overview'
+import Skills from './skills-section'
 
 export default function Content(props) {
   const { data } = props
@@ -12,6 +13,7 @@ export default function Content(props) {
         className='pt-24 sm:pt-32 xl:mx-auto xl:max-w-7xl xl:px-8'
       >
         <Overview />
+        <Skills />
       </section>
     </>
   )
---
+++ src/features/portfolio/content/skills-section/index.jsx
@@ -0,0 +1,9 @@
+const Skills = (props) => {
+  const { _ = {} } = props
+  return (
+    <div className='use tw utility classes here'>
+    </div>
+  )
+}
+
+export default Skills
