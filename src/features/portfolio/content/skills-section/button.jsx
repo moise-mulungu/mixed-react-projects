7b84edc6ddd50f1ea7140ea8f68f47ee2e94bebe
moise <moisemlg90@gmail.com>
2023-03-22 11:13:11 +0200

    handle onClick event

--- src/features/portfolio/content/skills-section/button.jsx
+++ src/features/portfolio/content/skills-section/button.jsx
@@ -1,11 +1,16 @@
 const Button = () => {
+  const handleClick = () => {
+    alert('You clicked me!')
+  }
+
   return (
-      <button
-        type="button"
-        className="rounded-md bg-indigo-600 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        Button text
-      </button>
+    <button
+      onClick={handleClick}
+      type="button"
+      className="rounded-md bg-indigo-600 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      Get my Resume
+    </button>
   )
 }
 
