4d25d8ca6480a1dbe00c0b1684adec0eca0b9bcc
moise <moisemlg90@gmail.com>
2023-03-22 12:32:52 +0200

    Create new constants

--- src/constants/portfolio/index.js
+++ src/constants/portfolio/index.js
@@ -36,3 +36,9 @@ export const contentOverviewParagraphText =
 
 export const contentAboutHeaderText =
   "Hello I am a software developer, I can help you build a product, feature or website. look through some of my work and experience. if you like what you see and have a project you need coded don't hesitate to contact me."
+
+export const frameworks = ['React', 'Ruby on Rails', 'NodeJS', 'BooStrap', 'Capybara', 'RSpec', 'Selenium', 'PostgreSQL']
+
+export const languages = ['JavaScript', 'Ruby', 'SQL']
+
+export const otherSkills = ['HTML', 'CSS', 'SASS', 'GraphQL', 'Git', 'GitHub', 'CodeLab', 'CodeLit', 'Terminal', 'VSCode', 'Jira', 'Slack']
