24b475bcb380f04e0143ec74354712f12540b591
moise <moisemlg90@gmail.com>
2023-03-23 14:17:52 +0200

    Try to compile

--- src/features/portfolio/content/project-section/project-cards.jsx
+++ src/features/portfolio/content/project-section/project-cards.jsx
@@ -1,30 +1,29 @@
-import { projectCards } from "../../../../constants/portfolio";
-import CardTexts from "./card-texts";
+import { projectCards } from '../../../../constants/portfolio'
+import CardTexts from './card-texts'
 const ProjectCard = () => {
-    return (
-        <div className="tailwind-class-uploading">
-            <CardTexts />
-            <ul>
-                {projectCards.map((id, title, description, image, url, stacks) => {
-                    return (
-                        <li key={id}>
-                            <title>{title}</title>
-                            <p>{description}</p>
-                            <img src={image} alt="" />
-                            <p>{url}</p>
-                            {stacks.map((stack) => {
-                                return <p key={stack}>{stack}</p>;
-                            }
-                            // i am getting undefined whe trying to map through stacks
-                            )}
-                        </li>
-                    
-                    );
-                }
-                )}
-            </ul>
-        </div>
-    );
-};
+  return (
+    <div className="tailwind-class-uploading">
+      <CardTexts />
+      <ul>
+        {projectCards.map((id, title, description, image, url, stacks) => {
+          return (
+            <li key={id}>
+              <title>{title}</title>
+              <p>{description}</p>
+              <img src={image} alt="" />
+              <p>{url}</p>
+              <div>
+                {stacks?.map((stack) => {
+                  ;<li key={stack}>{stack}</li>
+                })}
+              </div>
+              // i am getting undefined whe trying to map through stacks
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
 
-export default ProjectCard;
+export default ProjectCard
