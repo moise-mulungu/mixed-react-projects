84baadcb2bbd1492a93df36ac09c5680a57a1609
moise <moisemlg90@gmail.com>
2023-03-28 14:21:41 +0200

    Create a sidenote component

---
+++ src/features/component-examples/side-note/app.jsx
@@ -0,0 +1,11 @@
+import Sidenote from './side-note'
+
+function App() {
+  return (
+    <>
+      <Sidenote title="I'm a sidenote!">I contain some text!</Sidenote>
+    </>
+  )
+}
+
+export default App
