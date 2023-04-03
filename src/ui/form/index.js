948af2add2b8afe90f52ec7dbe587c0e46bc030f
moise <moisemlg90@gmail.com>
2023-04-03 13:01:14 +0200

    Add a search form

--- src/ui/form/index.js
+++ src/ui/form/index.js
@@ -1,4 +1,3 @@
-
 /* 
 
 usage:
@@ -6,6 +5,19 @@ import { Button } from '@ui/form'
 
 */
 
-export {default as Button} from './button'
-export {default as Dropdown} from './dropdown'
-export {default as TextInput} from './text-input'
+export { default as Button } from './button'
+export { default as Dropdown } from './dropdown'
+export { default as TextInput } from './text-input'
+
+import SearchForm from './search-form'
+
+function App() {
+  // This function is a placeholder.
+  function runSearch(searchTerm) {
+    window.alert(`Searched for: ${searchTerm}`)
+  }
+
+  return <SearchForm runSearch={runSearch} />
+}
+
+export default App
