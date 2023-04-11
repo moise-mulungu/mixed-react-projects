d58f857bee097742a7d7f6aaa5f8294fb2ea424d
moise <moisemlg90@gmail.com>
2023-04-11 15:53:53 +0200

    Add comment

--- src/ui/form/select/select-colors.jsx
+++ src/ui/form/select/select-colors.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 //(in progress) DM: todoMM: try passing the colors, the legend, and the initial color as props, so that this is more generic
 // DM: i have not read about transitioning from props to state. i will do that tomorrow as there is a lesson called "props vs state" in the joy-of-react course
-function SelectColors(props) {
+function SelectColors({ colors, legend, initialColor = 'red' }) {
+  // ???DM: after passing colors, legend, and initialColor as props, where to call the colors , and legend. the initialColor is called in the useState, what about the other two?
   // DM: sounds good, this is a hint:
-  const { colors, legend, initialColor = 'red' } = props
+  // const { colors, legend, initialColor = 'red' } = props
   const [selectedOption, setSelectedOption] = React.useState(initialColor)
 
   return (
