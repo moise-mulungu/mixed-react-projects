343d8863998246b990606e55ea95ca01559a8f26
mcdevv <dmckeeve9@gmail.com>
2023-06-05 12:13:47 -0600

    are-they-the-same2.js

--- project-info/teamdm/training/code-wars-challenges/currently-working/are-they-the-same-2.js
+++ project-info/teamdm/training/code-wars-challenges/currently-working/are-they-the-same-2.js
@@ -6,12 +6,14 @@ function comp(numbers, squaredNumbers) {
   // howtojs: performance:: sorting avoids having a nested loop (a loop inside a loop), which is exponentially expensive. Sorting has it's costs, but you can assume that the JS Engine is efficient at sorting.
   const sortedNumbers = numbers.sort((a, b) => a - b)
   const sortedSquaredNumbers = squaredNumbers.sort((a, b) => a - b)
+
   if (
     (sortedNumbers === null && sortedSquaredNumbers.length === 0) ||
     (sortedNumbers.length === 0 && sortedSquaredNumbers === null)
   ) {
     return false
     // MM: toDM: here is where i want to check if the arrays are empty or null and then return false
+    // DM: todoMM: you can do these validation before the sort (more efficient). The new test I added below may give a clue as to how to make it work. Your conditions are very specific
   }
   for (let i = 0; i < sortedNumbers.length; i++) {
     if (sortedNumbers[i] * sortedNumbers[i] !== sortedSquaredNumbers[i]) {
@@ -22,6 +24,8 @@ function comp(numbers, squaredNumbers) {
 }
 
 comp([], null) // false
+// DM: todoMM: make sure your solution works with this new test on the line below
+comp(null, []) // false
 
 comp([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361]) // true
 comp([121, 144, 19, 161, 19, 144, 19, 11], [132, 14641, 20736, 361, 25921, 361, 20736, 361]) // false
@@ -33,4 +37,6 @@ comp([1], [1]) // true
 comp([1], [2]) // false
 
 // MM: toDM: The code above works for some almost all test cases(212), except two(2) in the code wars challenge.
-// DM: todoMM: reproduce those 2 failed ('Attempt' button) tests here and try to solve it, so that I can see. What is the error output? Does that error message give you clues?; yes, it does. the two only testings are to compare when array is null and array is empty or vice versa. I added the condition above to check for that, but it does not work.
+// DM: todoMM: reproduce those 2 failed ('Attempt' button) tests here and try to solve it, so that I can see. What is the error output? Does that error message give you clues?;
+// MM: toDM: yes, it does. the two only testings are to compare when array is null and array is empty or vice versa. I added the condition above to check for that, but it does not work.
+// DM: todoMM: Look at the error output, both in codewars.com 'Attempt' button (looking at the "output" tab in the left pane) - OR - run in the node REPL to see error message there also. Error messages will tell you what the problem is.
