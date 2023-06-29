3f77469a5b3bffa11a855bfb7280d49166bde68c
mcdevv <dmckeeve9@gmail.com>
2023-06-29 11:35:50 -0600

    review of reduce video

--- project-info/teamdm/tech/javascript/array-methods/array-method-cheatsheets/reduce-cheatsheet.js
+++ project-info/teamdm/tech/javascript/array-methods/array-method-cheatsheets/reduce-cheatsheet.js
@@ -29,61 +29,88 @@
 // good!
 /* howtojs: array: object:: turn array into: object; initialize with empty object as the initial value */
 // turn array into: object
-;['a', 'b', 'c', 'a', 'b']
-  .reduce((acc, cur) => {
-    if (acc[cur]) {
-      acc[cur]++
-    } else {
-      acc[cur] = 1
-    }
-    return acc
-  }, {})
-
-  [
-    // expected result: { a: 2, b: 2, c: 1 }
-    /* 
+;['a', 'b', 'c', 'a', 'b'].reduce((acc, cur) => {
+  if (acc[cur]) {
+    acc[cur]++
+  } else {
+    acc[cur] = 1
+  }
+  return acc
+}, {})
+// expected result: { a: 2, b: 2, c: 1 }
+/* 
 (done)DM: make a demo video explaining how this works.
-(done)DM: todoMM: typically comments refer to the code BELOW the comment. You did well with the demo video for the code above. Do another one for more practice. The below code will require more of the vocabulary in the list below.
 Vocab:
-property access operator - "." as in array.reduce or obj.propertyName
-callback function AKA callback
-initialize a variable or object property
-empty object
-shorthand property names - as in { cur, acc } is 'shorthand' for { cur: cur, acc: acc }
-increment - ex: numberVariable++
-array literal - ex: ['a', 'b', ... ]
-parameters - ex: to the callback function
-accumulator - acc, the 2nd parameter to the callback
-current value - cur
-computed property access operator - "[]" as in acc[cur]
+* property access operator - "." as in array.reduce or obj.propertyName
+* callback function AKA callback
+* to initialize a variable or object property; ex: const myVar = 1; obj.prop = 1;
+* empty object {}
+* shorthand property names - as in { cur, acc } is 'shorthand' for { cur: cur, acc: acc }
+* to increment - ex: numberVariable++
+* to iterate - ex: to iterate an array, the current iteration of the reduce loop
+* array literal - ex: ['a', 'b', ... ]
+* parameters of the callback function
+  * acc - accumulator, which was initialized as the 2nd argument to .reduce()
+  * cur - value is the element of the current iteration of the array
+  * idx - index of the current iteration of the array
+  * src - source (contains the array that is being iterated by .reduce())
+  * note: parameters and arguments are the same thing, but the correct word depends on context
+    * parameter: context is inside a function 
+    * argument: context is when you are CALLING a function, you pass arguments myFunc(myArg1, myArg2)
+* accumulator - acc, the 2nd parameter to the reduce() function (sorry, I had 'callback' here previously, that was wrong)
+* current value - ex: cur is the current value of the current iteration of the array
+* computed property access operator - "[]" as in acc[cur]
 
 */
-    // howtojs: array: reduce:: turn array into: complex object; group by property
-    // the link to the demo: https://www.youtube.com/watch?v=2sRHiskWLqo
-    ('a', 'b', 'c', 'a', 'b')
-  ].reduce(
-    (acc, cur, idx) => {
-      acc[cur].count++
-      acc[cur].indexes.push(idx)
-      // console.log({ acc, cur, idx })
-      console.log({ cur, idx })
-      return acc
+// howtojs: array: reduce:: turn array into: complex object; group by property
+// the link to the demo: https://www.youtube.com/watch?v=2sRHiskWLqo
+/* 
+DM: good job. I think making videos is good practice in speaking about code and also let's me know any gaps to fill in your knowledge.
+A few things I noticed while listening:
+* you said "required to use reduce, not an array method" - can you tell me another array method that works better than reduce? :) It was 'let' and a for loop that you were not allowed to use. 
+* nice typing while talking
+	* recommend you type your code BEFORE explaining the logic. The logic you explained WHILE you were typing was most understandable. Logic you discussed BEFORE was really hard to follow. It is always better to have something you can point at with your mouse cursor - this reduces the number of words you have to say to explain. 
+* don't mention Copilot or turn it off
+* "initialized to 1" not "equalized to 1"
+DM: todoMM: It will be good to record the video again. But, this time, before you record, write down what you will say here below. Tomorrow I will edit it, which you will learn from seeing the diffs. Then, after I have edited it, record another video. Write what you will say like this, using bullet points.
+
+Introduction
+* make a point
+* another point
+The problem
+* point
+* point
+The solution
+* point
+  * sub-point (ok to indent as needed)
+* point
+Wrap up
+* ...
+
+*/
+;['a', 'b', 'c', 'a', 'b'].reduce(
+  (acc, cur, idx) => {
+    acc[cur].count++
+    acc[cur].indexes.push(idx)
+    // console.log({ acc, cur, idx })
+    console.log({ cur, idx })
+    return acc
+  },
+  {
+    a: {
+      count: 0,
+      indexes: [],
     },
-    {
-      a: {
-        count: 0,
-        indexes: [],
-      },
-      b: {
-        count: 0,
-        indexes: [],
-      },
-      c: {
-        count: 0,
-        indexes: [],
-      },
-    }
-  )
+    b: {
+      count: 0,
+      indexes: [],
+    },
+    c: {
+      count: 0,
+      indexes: [],
+    },
+  }
+)
 /*
 result:
 {
