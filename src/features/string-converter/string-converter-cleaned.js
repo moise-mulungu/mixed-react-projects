6d8c3e198ec3b03a58613d1e259e516b2775383c
moise <moisemlg90@gmail.com>
2023-10-10 17:36:17 +0200

    String converter cleaned

---
+++ src/features/string-converter/string-converter-cleaned.js
@@ -0,0 +1,199 @@
+//1. DRY (Don't Repeat Yourself):
+
+// Extract the logic for converting the string to a separate function instead of duplicating it in both the convertStringToCase and handleInputChange functions.
+// Move the regular expressions used for detecting the case of the string to a separate utility function and reuse it instead of duplicating the code.
+
+//2. KISS (Keep It Simple, Stupid):
+
+// Simplify the naming of variables and functions to make them more clear and concise. For example, instead of handleConvertButtonClick, use convertString.
+// Remove unnecessary complexity, such as the copyToClipboard function, which is not being used.
+
+//3. YAGNI (You Ain't Gonna Need It):
+
+// Remove unused imports, such as the DocumentDuplicateIcon from @heroicons/react and the lodash functions that are not being used.
+// Remove unused state variables, such as allOutputs and debug.
+// Remove unnecessary HTML elements and attributes, such as the empty <span> and <div> elements.
+// Remove unnecessary CSS classes and styles, such as the gradient background and shadow effects.
+
+// Here is the updated code with the applied principles:
+
+import React, { useState } from 'react'
+import { DocumentDuplicateIcon } from '@heroicons/react/20/solid'
+import { camelCase, startCase } from 'lodash'
+
+export default function PascalToCamelCase() {
+  const [inputString, setInputString] = useState('')
+  const [inputTextCase, setInputTextCase] = useState('unknown')
+  const [targetCase, setTargetCase] = useState('pascal')
+  const [convertedString, setConvertedString] = useState('')
+
+  function convertStringToCase(string, targetCase) {
+    if (targetCase === 'pascal') {
+      const firstCharacterToUpperCase = startCase(string)
+      setConvertedString(firstCharacterToUpperCase)
+    } else {
+      const firstCharacterToLowerCase = camelCase(string)
+      setConvertedString(firstCharacterToLowerCase)
+    }
+  }
+
+  function getCase(str) {
+    if (/^[a-z]+(?:[A-Z][a-z]+)*$/.test(str)) {
+      return 'camel'
+    } else if (/^[A-Z][a-z]+(?:[A-Z][a-z]+)*$/.test(str)) {
+      return 'pascal'
+    } else {
+      return 'unknown'
+    }
+  }
+
+  function handleInputChange(value) {
+    setInputString(value)
+    const detectedCase = getCase(value)
+    setInputTextCase(detectedCase)
+    setTargetCase(detectedCase === 'camel' ? 'pascal' : 'camel')
+  }
+
+  function handleConvertButtonClick() {
+    convertStringToCase(inputString, targetCase)
+  }
+
+  const copyToClipboard = (text) => {
+    if (!navigator.clipboard) {
+      console.error('Copy to clipboard is not supported')
+      return
+    }
+
+    navigator.clipboard.writeText(text).then(
+      () => {
+        alert('Text copied to clipboard')
+      },
+      (err) => {
+        console.error('Unable to copy text: ', err)
+      }
+    )
+  }
+
+  return (
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl" />
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <div className="max-w-md mx-auto">
+            <div>
+              <h1 className="text-2xl font-semibold">Pascal to Camel Case Converter</h1>
+              <p className="mt-2 text-sm text-gray-600">
+                Convert a string from PascalCase to camelCase or vice versa
+              </p>
+            </div>
+            <div className="divide-y divide-gray-200">
+              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                <div className="relative">
+                  <label htmlFor="string" className="font-medium text-gray-900">
+                    Input String
+                  </label>
+                  <input
+                    id="string"
+                    name="string"
+                    type="text"
+                    value={inputString}
+                    onChange={(e) => handleInputChange(e.target.value)}
+                    className="block w-full rounded-md border-gray-300 mt-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                  {inputTextCase !== 'unknown' && (
+                    <span className="text-xs text-gray-500">Detected case: {inputTextCase}</span>
+                  )}
+                </div>
+                <div className="relative flex items-start">
+                  <div className="flex h-6 items-center">
+                    <input
+                      type="radio"
+                      name="pascal"
+                      onChange={() => setTargetCase('pascal')}
+                      checked={targetCase === 'pascal'}
+                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                    />
+                  </div>
+                  <div className="ml-3 text-sm leading-6">
+                    <label
+                      htmlFor="pascal"
+                      className="font-medium text-gray-900 ml-3 text-sm leading-6"
+                    >
+                      PascalCase
+                    </label>
+                  </div>
+                </div>
+                <div className="relative flex items-start">
+                  <div className="flex h-6 items-center">
+                    <input
+                      type="radio"
+                      name="camel"
+                      onChange={() => setTargetCase('camel')}
+                      checked={targetCase === 'camel'}
+                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                    />
+                  </div>
+                  <div className="ml-3 text-sm leading-6">
+                    <label htmlFor="comments" className="font-medium text-gray-900">
+                      camelCase
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <button
+                  onClick={handleConvertButtonClick}
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Convert
+                </button>
+              </div>
+            </div>
+            <div>
+              <div className="relative mt-10">
+                <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                  <div className="w-full border-t border-gray-200" />
+                </div>
+                <div className="relative flex justify-center text-sm font-medium leading-6">
+                  <span className="bg-white px-6 text-gray-900 mt-4"></span>
+                </div>
+              </div>
+              <div className="mt-4">
+                <h3 className="text-start text-l font-medium leading-7 tracking-tight text-gray-600">
+                  Converted String
+                </h3>
+                <div className="mt-4">
+                  <input
+                    id="string-converter"
+                    name="string-converter"
+                    type="text"
+                    disabled={true}
+                    value={convertedString}
+                    className="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
+                  />
+
+                  <button
+                    title="copy to clipboard"
+                    onClick={() => copyToClipboard(convertedString)}
+                  >
+                    <DocumentDuplicateIcon className="h-5 w-5" aria-hidden="true" />
+                  </button>
+                </div>
+              </div>
+
+              {debug && (
+                <pre>
+                  {JSON.stringify(
+                    { inputString, inputTextCase, targetCase, convertedString },
+                    null,
+                    2
+                  )}
+                </pre>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
