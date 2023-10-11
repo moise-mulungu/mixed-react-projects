d5d3611eb43194cd159a3be754a45056e7b29d5c
moise <moisemlg90@gmail.com>
2023-10-11 17:51:31 +0200

    Weather styling

--- src/features/weather-forecast/axios/index.js
+++ src/features/weather-forecast/axios/index.js
@@ -4,7 +4,10 @@ import Weather from './weather'
 const OpenWeatherMap = () => {
   return (
     <div>
-      <h1>Weather OpenWeatherMap</h1>
+      {/* <h1>Weather OpenWeatherMap</h1> */}
+      <div className="border-b border-gray-200 pb-5 sm:flex sm:items-center sm:justify-between">
+        <h3 className="text-base font-semibold leading-6 text-gray-900">Weather Forecast</h3>
+      </div>
       <Weather />
     </div>
   )
--- src/features/weather-forecast/index.js
+++ src/features/weather-forecast/index.js
@@ -1,8 +1,8 @@
 //(done) DM: todoMM: rename the directory from weather-app to weather-forecast
 
 import React from 'react'
-import Forecast from './components/forecast/forecast'
-import App from './weather-forecast-example/App'
+// import Forecast from './components/forecast/forecast'
+// import App from './weather-forecast-example/App'
 import Divider from '@/ui/divider'
 //(in progress) DM: todoMM: change the name of the file or the default import name so that they are the same. This gets confusing to know what something is and where it comes from
 import OpenWeatherMap from './axios/index'
@@ -10,14 +10,7 @@ import OpenWeatherMap from './axios/index'
 function WeatherForecast() {
   return (
     <div>
-      <header>
-        <h1>React Weather WeatherForecastPage</h1>
-      </header>
       <main>
-        <Forecast />
-        <Divider />
-        <App />
-        <Divider />
         <OpenWeatherMap />
       </main>
       <footer>Page created by Moise Mulungu</footer>
