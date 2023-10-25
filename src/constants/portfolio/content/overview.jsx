f4bc6702d93ef1a765bee98f35a328f5b7905583
moise <moisemlg90@gmail.com>
2023-10-25 17:56:51 +0200

    Portfolio UI

--- src/constants/portfolio/content/overview.jsx
+++ src/constants/portfolio/content/overview.jsx
@@ -1,3 +1,3 @@
-export const contentOverviewHeaderText = 'Hey there, I am Moise. a software developer'
+export const contentOverviewHeaderText = 'I am Moîse, a software developer'
 export const contentOverviewParagraphText =
   'I can help you build a product, feature or website Look through some of my work experience! You like what you see and have a project you need coded, do not hesitate to contact me.'
