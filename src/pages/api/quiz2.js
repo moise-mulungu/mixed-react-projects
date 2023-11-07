aeea3244782e58b0d0faad5012ff858c433e32e7
mcdevv <dmckeeve9@gmail.com>
2023-11-07 10:47:36 -0700

    Tue

--- src/pages/api/quiz2.js
+++ src/pages/api/quiz2.js
@@ -5,11 +5,12 @@ import axios from 'axios'
 export default async (req, res) => {
   // temporary until you get the external API working. See output at: http://localhost:3005/api/quiz
   //   return res.status(200).json(data) // the rest of the code below never runs if you return early.
-  //(done) DM: todoMM: good. use the lodash isEmpty like this: if (isEmpty(req.query)) { ... } // because is more readable and handles more situations, i.e., if req.query is undefined or {} empty object
+  //(done) DM: good. use the lodash isEmpty like this: if (isEmpty(req.query)) { ... } // because is more readable and handles more situations, i.e., if req.query is undefined or {} empty object
   // if (Object.keys(req.query).length === 0) {
   const isEmpty = require('lodash/isEmpty')
   if (isEmpty(req.query)) {
-    //(done) DM: todoMM: good. should this be 500 (Error?). If you chose 400 specifically let me know your reasoning
+    //(done) DM: good. should this be 500 (Error?). If you chose 400 specifically let me know your reasoning
+    // DM: todoMM: back to 400 - don't always take my advice as gospel, esp if I give it in the form of a question, like I did above. Managers expect you to tell them when they are wrong. BTW, in an interview a manager asked me what I would do if  he we're to give me incorrect orders.
     return res.status(500).send('Bad Request: No query parameters provided')
   }
   const { category } = req.query
@@ -17,6 +18,7 @@ export default async (req, res) => {
 
   if (!category) {
     // return res.status(400).json({ error: 'City is not provided' })
+    // DM: 400 is Bad Request. 500 is Internal Server Error. 400 is more appropriate here, and above despite what I told you yesterday (although I did ask :)
     return res.status(400).send('Bad Request: category not specified')
   }
 
@@ -31,7 +33,62 @@ export default async (req, res) => {
     const data = response.data
     console.log('data:', { data })
 
-    res.status(200).json(data)
+    /* 
+      data transformation 
+
+      FROM
+      {
+        id: 351,
+        question: 'What is a composite key?',
+        description: null,
+        answers: {
+          answer_a: 'its is a key that is defined as the primary key in another table',
+          answer_b: 'it is a key that uniquely identifies a record in a database',
+          answer_c: 'its is an optional key and allows null values',
+          answer_d:
+            'it is a primary key that consists of more than one field that uniquely identifies a record',
+          answer_e: null,
+          answer_f: null,
+        },
+        multiple_correct_answers: 'false',
+        correct_answers: {
+          answer_a_correct: 'false',
+          answer_b_correct: 'false',
+          answer_c_correct: 'false',
+          answer_d_correct: 'true',
+          answer_e_correct: 'false',
+          answer_f_correct: 'false',
+        },
+        correct_answer: 'answer_a',
+        explanation: null,
+        tip: null,
+        tags: [
+          {
+            name: 'MySQL',
+          },
+        ],
+        category: 'SQL',
+        difficulty: 'Easy',
+      }
+
+      TO (you want to return the SQL questions in the following format)
+
+      {
+        questionId: 1,
+        question: 'What does HTML stand for?',
+        correctAnswer: 'Hyper Text Markup Language',
+        answerChoices: [
+          'Hyper Text Preprocessor',
+          'Hyper Text Markup Language',
+          'Hyper Text Multiple Language',
+          'Hyper Tool Multi Language',
+        ],
+      }
+    */
+    // DM: todoMM: uncomment the next line, put cursor at the end of that line, then press Enter, and see what Copilot suggests. For me, it was a good start, but there was at least one error. Console.log it and you'll figure it out.
+    // const transformedData = data.map((question) => {
+
+    const datam = res.status(200).json(data)
   } catch (error) {
     console.error({ error, errorMessage: error.message })
     res.status(500).json({ error: 'Unable to fetch quiz data', message: error.message })
