919aa234a99b56d32cb8969f7d7bd241d944b18e
moise <moisemlg90@gmail.com>
2024-02-25 21:35:15 +0200

    daily reports

--- src/pages/firebase-subcollections/index.jsx
+++ src/pages/firebase-subcollections/index.jsx
@@ -1,4 +1,4 @@
-// DM: todoMM: this doesnt exist; also put this in the src/features/firebase-subcollections folder as usual
+//(in progress) DM: todoMM: this doesnt exist; also put this in the src/features/firebase-subcollections folder as usual
 import App from '@/firebase-subcollections/src/App'
 // DM: todoMM: always explain non-working or incomplete code. what is this? what status is it in. what are the plans?
 export default function FirebaseSubcollections() {
