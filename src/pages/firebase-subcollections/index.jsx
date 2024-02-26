2770410c6a0cd48fd7a501e3c420494f59dfadc1
moise <moisemlg90@gmail.com>
2024-02-26 18:42:02 +0200

    firebase-subcollections-tutorial

--- src/pages/firebase-subcollections/index.jsx
+++ src/pages/firebase-subcollections/index.jsx
@@ -1,6 +1,7 @@
-//(in progress) DM: todoMM: this doesnt exist; also put this in the src/features/firebase-subcollections folder as usual
-import App from '@/firebase-subcollections/src/App'
+//(done) DM: todoMM: this doesnt exist; also put this in the src/features/firebase-subcollections folder as usual
+import App from '../../features/firebase-subcollections/src/App'
 // DM: todoMM: always explain non-working or incomplete code. what is this? what status is it in. what are the plans?
+// MM: the problem was when pushing the code to the repo, the folder was not pushed, i faced the submodules issue, when pushing the code to the repo, i will fix it and push it again
 export default function FirebaseSubcollections() {
   return <App />
 }
